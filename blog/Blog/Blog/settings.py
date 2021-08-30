a9cbce67bfb946c44ced26900685bb5147b80582
Antônio Bruno Kremer Lamim <63618595+BrunoKremer@users.noreply.github.com>
2021-08-30 17:27:20 -0300

    Arrumar templates, .gitignore e settings

--- blog/Blog/Blog/settings.py
+++ blog/Blog/Blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ydx%#3elkt6p1$amcu9jfzssu%0&&*0o^=g2ws!(fqmf983_^h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,3 +147,6 @@ REST_FRAMEWORK = {
 }
 
 CKEDITOR_UPLOAD_PATH = 'uploades/'
+
+with open("Blog/local_settings.py") as infile:
+    exec(infile.read())
