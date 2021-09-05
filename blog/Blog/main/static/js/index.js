4ffd87a614fc8b5daf9a4b383ab28354d9782c5a
Antônio Bruno Kremer Lamim <brunokremer022@gmail.com>
2021-09-05 15:41:59 -0300

    Layout
    Feito layout de indicação com template de indicação de cursos e livros

---
+++ blog/Blog/main/static/js/index.js
@@ -0,0 +1,29 @@
+const $responsiveCarousel = document.querySelector(".js-carousel--responsive");
+
+new Glider($responsiveCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  duration: 0.25,
+  dots: ".js-carousel--responsive-dots",
+  arrows: {
+    prev: ".js-carousel--responsive-prev",
+    next: ".js-carousel--responsive-next",
+  },
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+  ],
+});
