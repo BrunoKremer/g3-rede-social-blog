36cc504801811f0713077b30bfb30c2434f04357
Antônio Bruno Kremer Lamim <brunokremer022@gmail.com>
2021-09-25 19:34:11 -0300

    Views e outras atts

--- blog/Blog/social/views.py
+++ blog/Blog/social/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse, request, response
 from django.views import generic
 from .models import Publicacao
 from django.urls.base import reverse_lazy
-from .forms import Publicacao_form 
+from .forms import Publicacao_form
 
 def feed(request):
     posts = Publicacao.objects.all()
+    comentarios = Comentario.objects.all()
+    if request.method == 'POST':
+        form_comentario = Comentario_publi(request.POST)
+        if form_comentario.is_valid():
+            data = Comentario()
+            data.comentario = form_comentario.cleaned_data['comentario']
+            data.foto = form_comentario.cleaned_data['foto']
+            data.usuario_id = request.user.id
+            data.publicacao_id = 4
+            data.save()
+    else:
+        form_comentario = Comentario_publi()
     if request.method == 'POST':
        form = Publicacao_form(request.POST)
        if form.is_valid():
