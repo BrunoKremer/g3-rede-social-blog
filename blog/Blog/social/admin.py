c46a98771f2c0f1b370dd830cfbd8fcb8694ea0d
JeanFranca12 <jeancarlossantos12@gmail.com>
2021-09-27 15:33:37 -0300

    Update Models
    alteracao

--- blog/Blog/social/admin.py
+++ blog/Blog/social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Publicacao, Comentario
+from .models import Publicacao
 
 admin.site.register(Publicacao)
-admin.site.register(Comentario)
+# admin.site.register(Comentario)
